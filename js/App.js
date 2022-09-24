0c511bfed7c8f7d1dcaa46daadd05642c0a473dc
japondesigner <japondesigner@gmail.com>
2022-09-25 01:17:00 +0200

    cambios de precios final

--- js/App.js
+++ js/App.js
@@ -3,16 +3,17 @@
     new Vue({
       el:'#app',
       data: {
-          total:323,
+          total:0,
+          totalmes:0,
           countweblandignpage:false,
           countWebDinamica:false,
           countWebEcommerce:false,
           countWebAmedida:false,
-          countWordpress:false,
+          countycode:false,
           countSEOonPage:false,
           countGoogleAnalytics:false,
           countApp:false,
-          countAppBD:false,
+          countemail:false,
           countSEOdecompetencia:false,
           countBrandBook:false,
           countBranding:false,
@@ -32,51 +33,51 @@
       },
       WebDinamica: function () {
         this.countWebDinamica = true
-        return this.total = this.total + 300
+        return this.total = this.total + 500
       },
       WebEcommerce: function () {
         this.countWebEcommerce = true
-        return this.total = this.total + 300
+        return this.total = this.total + 800
       },
       WebAmedida: function () {
         this.countWebAmedida = true
-        return this.total = this.total + 300
+        return this.total = this.total + 900
       },
-      Wordpress: function () {
-        this.countWordpress = true
+      ycode: function () {
+        this.countycode = true
         return this.total = this.total + 300
       },
       SEOonPage: function () {
         this.countSEOonPage = true
-        return this.total = this.total + 300
+        return this.total = this.total + 80
       },
       GoogleAnalytics: function () {
         this.countGoogleAnalytics = true
-        return this.total = this.total + 300
+        return this.total = this.total + 80
       },
       App: function () {
         this.countApp = true
-        return this.total = this.total + 300
+        return this.total = this.total + 100
       },
-      AppBD: function () {
-        this.countAppBD = true
+      email: function () {
+        this.countemail = true
         return this.total = this.total + 300
       },
       SEOdecompetencia: function () {
         this.countSEOdecompetencia = true
-        return this.total = this.total + 300
+        return this.totalmes = this.totalmes + 50
       },
       BrandBook: function () {
         this.countBrandBook = true
-        return this.total = this.total + 300
+        return this.total = this.total + 600
       },
       Branding: function () {
         this.countBranding = true
-        return this.total = this.total + 300
+        return this.total = this.total + 100
       },
       Papeleria: function () {
         this.countPapeleria = true
-        return this.total = this.total + 300
+        return this.total = this.total + 400
       },
       Artepublicitario: function () {
         this.countArtepublicitario = true
@@ -84,19 +85,19 @@
       },
       Ediciondevideo: function () {
         this.countEdiciondevideo = true
-        return this.total = this.total + 300
+        return this.total = this.total + 700
       },
       Packaging: function () {
         this.countPackaging = true
-        return this.total = this.total + 300
+        return this.total = this.total + 600
       },
       disenodelogo: function () {
         this.countdisenodelogo = true
-        return this.total = this.total + 300
+        return this.total = this.total + 100
       },
       Ukiyo: function () {
         this.countUkiyo = true
-        return this.total = this.total + 300
+        return this.total = this.total + 80
       }
 
 
