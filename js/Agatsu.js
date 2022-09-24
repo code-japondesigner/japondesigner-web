64ae1b117aaaf0c090638bc0d3ff86781af86016
japondesigner <japondesigner@gmail.com>
2022-09-24 19:48:03 +0200

    cambios de logica pagos

---
+++ js/Agatsu.js
@@ -0,0 +1,16 @@
+
+    
+    new Vue({
+      el:'#Agatsu',
+      data: {
+          total:323
+      },
+      
+  methods: {
+
+      Weblandignpage: function () {
+          return this.total += 300
+      }
+
+    }
+  })
---
+++ js/App.js
@@ -0,0 +1,68 @@
+
+    
+    new Vue({
+      el:'#app',
+      data: {
+          total:323
+      },
+      
+  methods: {
+
+      weblandignpage: function () {
+          return this.total = this.total + 300
+      },
+      WebDinamica: function () {
+        return this.total = this.total + 300
+      },
+      WebEcommerce: function () {
+        return this.total = this.total + 300
+      },
+      WebAmedida: function () {
+        return this.total = this.total + 300
+      },
+      Wordpress: function () {
+        return this.total = this.total + 300
+      },
+      SEOonPage: function () {
+        return this.total = this.total + 300
+      },
+      GoogleAnalytics: function () {
+        return this.total = this.total + 300
+      },
+      App: function () {
+        return this.total = this.total + 300
+      },
+      AppBD: function () {
+        return this.total = this.total + 300
+      },
+      SEOdecompetencia: function () {
+        return this.total = this.total + 300
+      },
+      BrandBook: function () {
+        return this.total = this.total + 300
+      },
+      Branding: function () {
+        return this.total = this.total + 300
+      },
+      Papeleria: function () {
+        return this.total = this.total + 300
+      },
+      Artepublicitario: function () {
+        return this.total = this.total + 300
+      },
+      Ediciondevideo: function () {
+        return this.total = this.total + 300
+      },
+      Packaging: function () {
+        return this.total = this.total + 300
+      },
+      disenodelogo: function () {
+        return this.total = this.total + 300
+      },
+      Ukiyo: function () {
+        return this.total = this.total + 300
+      }
+
+
+    }
+  })
